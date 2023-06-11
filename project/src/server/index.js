3d9cd8c949571b90d263256274620c7970202b23
longbui98 <quanglongbui153@gmail.com>
2023-06-11 23:05:33 +0700

    Update set up api

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -1,11 +1,15 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+const dotenv = require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+dotenv.config({
+    path: './configure.env'
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
